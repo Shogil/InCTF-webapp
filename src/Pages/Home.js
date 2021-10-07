19ef47b5c4cb688fdbf8430212a1f3bbdf2ed85d
shogil <shogilbabu@gmail.com>
2021-10-07 09:05:22 +0530

    About folder added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Topbar from '../Components/Bars/Topbar'
 import Banner from '../Components/Banner/Banner'
+import About from '../Components/About/About'
 
 function Home() {
     return (
         <div>
             <Topbar/>
             <Banner/>
+            <About/>
         </div>
     )
 }
